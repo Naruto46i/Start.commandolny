ad4795d5f61992050763ec104581519ed64181ea
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2023-12-12 22:25:04 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6337844046:AAGFdgIBZ21XfQGtMc6_CEPSIDEHCG5zVOY')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 
