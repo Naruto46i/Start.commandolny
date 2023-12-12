a8effe07d37fab549002973094535f515d4a8137
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2023-12-12 22:23:57 +0530

    Create about.py

---
+++ plugins/about.py
@@ -0,0 +1,15 @@
+import os 
+from pyrogram import Client, filters
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
+from helper.database import botdata, find_one, total_user
+
+from helper.progress import humanbytes
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
+	await message.reply_text
